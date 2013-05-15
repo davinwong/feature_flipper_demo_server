6ba679d83268ed0e099845fa6d1bd7b986bcd1a7
davin <davin@tophat.com>
2013-05-15 12:31:30 -0400

    updating restapi view

--- restapi/views.py
+++ restapi/views.py
@@ -9,38 +9,8 @@ from restapi.models import Question, Answer
 import json
 from waffle.decorators import waffle_flag
 
-def test(request):
-	c = {}
 
-	if waffle.flag_is_active(request, 'feature1'):
-		c["feature11"] = "View Feature 1 is live!"
-	else:
-		c["feature11"] = "View Feature 1 is not available right now"
-
-	# question module
-	users = User.objects.all()
-
-	c['question_array'] = []
-	if waffle.flag_is_active(request, 'question'):
-		for user in users:
-			questions = Question.objects.filter(user=user.id)
-			c['question_array'].append("")
-			c['question_array'].append(user.username + ":")
-			if questions:
-				for question in questions:
-					c['question_array'].append(question.question)
-			else:
-				c['question_array'].append("Hasn't asked any questions... sad.")
-
-	if waffle.flag_is_active(request, 'answer'):
-		c["answer_array"] = Answer.objects.all()
-
-	c['cookie_answer'] = request.COOKIES.get('dwf_answer')
-	c['cookie_question'] = request.COOKIES.get('dwf_question')
-	c['cookie_feature1'] = request.COOKIES.get('dwf_feature1')
-
-	return render_to_response('test.html',c, context_instance = RequestContext(request))
-
+# REST api for /feature/1/user/2/, returns True/False for whether given user should see given feature
 def feature_user(request, flag_id, user_id):
 
 	# if waffle-"everyone"-setting is already set
@@ -85,6 +55,7 @@ def feature_user(request, flag_id, user_id):
 	return HttpResponse(myjson)
 
 
+# sample payment feature
 @waffle_flag('payment')
 def payment(request, user_id, credit_card_number):
 	# fake payment processing
@@ -95,3 +66,37 @@ def payment(request, user_id, credit_card_number):
 		myjson = json.dumps({'payment': False})
 
 	return HttpResponse(myjson)
+
+
+# ignore
+def test(request):
+	c = {}
+
+	if waffle.flag_is_active(request, 'feature1'):
+		c["feature11"] = "View Feature 1 is live!"
+	else:
+		c["feature11"] = "View Feature 1 is not available right now"
+
+	# question module
+	users = User.objects.all()
+
+	c['question_array'] = []
+	if waffle.flag_is_active(request, 'question'):
+		for user in users:
+			questions = Question.objects.filter(user=user.id)
+			c['question_array'].append("")
+			c['question_array'].append(user.username + ":")
+			if questions:
+				for question in questions:
+					c['question_array'].append(question.question)
+			else:
+				c['question_array'].append("Hasn't asked any questions... sad.")
+
+	if waffle.flag_is_active(request, 'answer'):
+		c["answer_array"] = Answer.objects.all()
+
+	c['cookie_answer'] = request.COOKIES.get('dwf_answer')
+	c['cookie_question'] = request.COOKIES.get('dwf_question')
+	c['cookie_feature1'] = request.COOKIES.get('dwf_feature1')
+
+	return render_to_response('test.html',c, context_instance = RequestContext(request))
